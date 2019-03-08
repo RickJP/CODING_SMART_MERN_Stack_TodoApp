c45c5830a531a0dc4b7c875493c530cbeed3692c
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-08 17:12:46 +0900

    created three components for routing

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class CreateTodo extends Component {
+    render() {
+        return (
+            <div>
+                <p>CreateTodo Component</p>
+            </div>
+        )
+    }
+}
